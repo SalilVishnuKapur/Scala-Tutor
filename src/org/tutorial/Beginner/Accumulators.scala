81a823452f7a2333d7731a472131de0858c2ffdb
Salil Vishnu Kapur <salilvishnukapur@gmail.com>
2017-02-21 22:44:53 +0530

    Added the pom and the accumulators example.

---
+++ src/org/tutorial/Beginner/Accumulators.scala
@@ -0,0 +1,16 @@
+package org.tutorial.Beginner
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object Accumulators {
+  def main(args :Array[String]) : Unit =
+  {
+    val sparkConf = new SparkConf()
+    .setAppName("Accumulators")
+    .set("spark.executor.extraJavaOptions", "-XX:+UseG1GC")
+    var sc = new SparkContext(sparkConf)
+    var accum = sc.Accumulators
+    sc.parallelize(0 until 10, 10).foreach(x => accum += 1)
+  }
+}
